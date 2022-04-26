267cf2382341a4b8480ca6e5388f932ef1a8d114
Frann0 <deengo113@gmail.com>
2022-04-26 20:30:44 +0200

    From login to sign up and back
    buttons work, possibly a better way of doing it, but leave it to refactor

--- app/src/main/java/com/de4aber/cappsule/SignUpActivity.kt
+++ app/src/main/java/com/de4aber/cappsule/SignUpActivity.kt
@@ -2,6 +2,7 @@ package com.de4aber.cappsule
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class SignUpActivity : AppCompatActivity() {
@@ -9,5 +10,12 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        val btnLogin: Button = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener { onClickLogin() }
+    }
+
+    private fun onClickLogin() {
+        finish()
     }
 }
