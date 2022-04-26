1b3953164b5c1ae889d617f9a3078523d19259a1
Frann0 <deengo113@gmail.com>
2022-04-26 20:03:26 +0200

    created context for xml files and added signup activity controller

---
+++ app/src/main/java/com/de4aber/cappsule/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package com.de4aber.cappsule
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
