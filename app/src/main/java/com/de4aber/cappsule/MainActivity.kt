1b9f253c8d756c9010be7f79d4c6e060443b8f18
EmilseBilse <emil875b@easv365.dk>
2022-04-27 17:43:10 +0200

    Encrypted password (klasse mangler da den er i .gitignore pga sikkerheds mæssige hensyn)

--- app/src/main/java/com/de4aber/cappsule/MainActivity.kt
+++ app/src/main/java/com/de4aber/cappsule/MainActivity.kt
@@ -1,23 +1,42 @@
 package com.de4aber.cappsule
 
+import android.R.attr.password
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.WindowManager
-import android.widget.Button
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.de4aber.cappsule.Utility.SecurityHelper
+import com.scottyab.aescrypt.AESCrypt
+import kotlinx.android.synthetic.main.activity_login.*
+import java.lang.Exception
+import java.security.GeneralSecurityException
+
 
 class MainActivity : AppCompatActivity() {
+    var securityHelper: SecurityHelper = SecurityHelper()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        val btnSignUp: Button = findViewById(R.id.btnSignUp)
 
         btnSignUp.setOnClickListener{onClickSignUp()}
+        btnLogin.setOnClickListener { onClickLogin() }
     }
 
     private fun onClickSignUp() {
         val intent = Intent(this, SignUpActivity::class.java)
         startActivity(intent);
+    }
+
+    private fun onClickLogin() {
+        val plainPW = editTextTextPassword.text.toString()
+        val username = editTextTextPersonName.text.toString()
+        val key = securityHelper.getEncryptionKey()
+        try {
+            val encryptedPassword = AESCrypt.encrypt(key, plainPW)
+            Log.d("TAG", encryptedPassword)
+        }        catch (e: GeneralSecurityException){
+            throw Exception("Key is most-likely not generated \n $e")
+        }
     }
 }
