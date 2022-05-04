427c32c7e1507d456263c5cc17a53a0af08f7d6d
EmilseBilse <emil875b@easv365.dk>
2022-05-04 22:26:14 +0200

    lavet ny activity

---
+++ app/src/main/java/com/de4aber/cappsule/AddLocationActivity.kt
@@ -0,0 +1,11 @@
+package com.de4aber.cappsule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddLocationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_location)
+    }
+}
