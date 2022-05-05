eb9bcf3e8c68fd1e208779e150e929113f1ef351
ceci6182 <ceci6182@easv365.dk>
2022-05-05 17:59:33 +0200

    virker i hvertfald

--- app/src/main/java/com/de4aber/cappsule/FriendActivity.kt
+++ app/src/main/java/com/de4aber/cappsule/FriendActivity.kt
@@ -3,11 +3,14 @@ package com.de4aber.cappsule
 import android.graphics.BlendMode
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.de4aber.cappsule.User.FriendListFragment
 import kotlinx.android.synthetic.main.activity_friend.*
 
 class FriendActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_friend)
+        supportFragmentManager.beginTransaction()
+            .add(R.id.friendScroll, FriendListFragment()).commit()
     }
 }
