c18f1a84d27820a83681b006a5960e5b9e576cc6
EmilseBilse <emil875b@easv365.dk>
2022-05-09 18:36:50 +0200

    jeg fjernede det jeg havdet lavet før for at få mappet til at virke og prøvede det samme igen nu virker det

--- app/src/main/java/com/de4aber/cappsule/AddLocationActivity.kt
+++ app/src/main/java/com/de4aber/cappsule/AddLocationActivity.kt
@@ -2,78 +2,47 @@ package com.de4aber.cappsule
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.ArrayAdapter
+
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
-import kotlinx.android.synthetic.main.activity_add_location.*
+import com.de4aber.cappsule.databinding.ActivityAddLocation2Binding
 
 class AddLocationActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private val TAG = "xyz"
-
-    private val EASV = LatLng(55.488230, 8.446936)
-    private val BAKER = LatLng(55.485386, 8.451585)
-    private val ROUND = LatLng(55.473939, 8.435959)
-
     private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityAddLocation2Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_location)
+
+        binding = ActivityAddLocation2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    fun onClickSave() {
-        TODO("Not yet implemented")
-    }
-
-    fun onClickBack() {
-        TODO("Not yet implemented")
-    }
-
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        val easvOptions = MarkerOptions().position(EASV).title("EASV")
-        val bakerOptions = MarkerOptions().position(BAKER).title("Baker").icon(
-            BitmapDescriptorFactory.fromResource(
-                R.drawable.ic_logo
-            )
-        )
-        val roundAboutOptions = MarkerOptions().position(ROUND).title("Round-about")
-        val easvMarker = mMap.addMarker(easvOptions)
-        mMap.addMarker(bakerOptions)
-        mMap.addMarker(roundAboutOptions)
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(EASV))
-        mMap.setOnMarkerClickListener(object : GoogleMap.OnMarkerClickListener {
-            override fun onMarkerClick(marker: Marker): Boolean {
-                if (marker.equals(easvMarker))
-                    Log.d(TAG, "EASV is clicked...")
-                return false
-            }
-        })
-
-        setupZoomlevels()
-
-    }
-
-    private fun setupZoomlevels() {
-        spinnerZoomLevel.adapter =
-            ArrayAdapter.createFromResource(
-                this,
-                R.array.zoomlevels,
-                android.R.layout.simple_spinner_dropdown_item
-            )
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
