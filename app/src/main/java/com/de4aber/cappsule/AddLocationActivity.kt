00be8e11e9dac480357e6d23d0de2a6a7f8a8852
EmilseBilse <emil875b@easv365.dk>
2022-05-09 20:43:29 +0200

    den zoomer ind på din lokation hvis du klikker på knappen

--- app/src/main/java/com/de4aber/cappsule/AddLocationActivity.kt
+++ app/src/main/java/com/de4aber/cappsule/AddLocationActivity.kt
@@ -1,12 +1,15 @@
 package com.de4aber.cappsule
 
+import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -16,7 +19,9 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.Task
 import kotlinx.android.synthetic.main.activity_add_location3.*
+import java.util.jar.Manifest
 
 class AddLocationActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -26,6 +31,8 @@ class AddLocationActivity : AppCompatActivity(), OnMapReadyCallback {
     private val ROUND = LatLng(55.473939, 8.435959)
 
     private lateinit var mMap: GoogleMap
+
+    private lateinit var client: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,10 +44,43 @@ class AddLocationActivity : AppCompatActivity(), OnMapReadyCallback {
 
         btnSaveLocation.setOnClickListener { Save() }
         btnLocationBack.setOnClickListener { Back() }
+        btnLocationZoomCurrent.setOnClickListener { getCurrentLocation() }
+
+        //Initialize fused location
+        client = LocationServices.getFusedLocationProviderClient(this)
+
+
+        //ZoomOnCurrentLocation()
 
     }
 
+    private fun ZoomOnCurrentLocation(coords: LatLng) {
+        val level: Int = spinnerZoomLevel.getSelectedItem().toString().toInt()
+        val viewPoint = CameraUpdateFactory.newLatLngZoom(coords, level.toFloat())
 
+        // zoomlevel 0..21, where 0 is the world and 21 is single street
+        Log.d(TAG, "Will zoom to easv to level $level")
+        mMap.animateCamera(viewPoint)
+    }
+
+    private fun getCurrentLocation(){
+        val task = client.lastLocation
+
+        if(ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 101)
+            return
+        }
+        task.addOnSuccessListener {
+            if(it != null){
+
+                var coords = LatLng(it.latitude, it.longitude)
+                ZoomOnCurrentLocation(coords)
+            }
+        }
+    }
 
     private fun Back() {
         finish()
