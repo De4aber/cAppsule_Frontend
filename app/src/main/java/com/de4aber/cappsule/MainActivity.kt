045e9dead557c78fbab882711b11449e6a57ee5b
Frann0 <deengo113@gmail.com>
2022-05-10 15:15:45 +0200

    Navbar color logic, and removed native android navigation

--- app/src/main/java/com/de4aber/cappsule/MainActivity.kt
+++ app/src/main/java/com/de4aber/cappsule/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.de4aber.cappsule
 
+import CapsuleSegmentFragment
+import MapSegmentFragment
 import android.content.Context
 import android.content.Intent
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import com.de4aber.cappsule.Friendlist.FriendListFragment
 import com.de4aber.cappsule.Friendlist.FriendSegmentFragment
 import com.de4aber.cappsule.Home.HomeSegmentFragment
 import com.de4aber.cappsule.User.LoggedUserViewModel
@@ -40,6 +42,12 @@ class MainActivity : AppCompatActivity() {
                 cappsule.setOnClickListener { onClickCapsule() }
             }
         }, userId)
+        window.decorView.apply {systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) }
     }
 
     private fun onClickCapsule() {
