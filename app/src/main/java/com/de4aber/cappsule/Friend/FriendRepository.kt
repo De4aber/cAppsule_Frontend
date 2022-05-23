8cc403ab7138a0bcc9aad57fc40f2d39e260b5bd
ceci6182 <ceci6182@easv365.dk>
2022-05-24 00:41:58 +0200

    rigtige capsules

--- app/src/main/java/com/de4aber/cappsule/Friend/FriendRepository.kt
+++ app/src/main/java/com/de4aber/cappsule/Friend/FriendRepository.kt
@@ -7,7 +7,6 @@ import androidx.lifecycle.MutableLiveData
 import com.de4aber.cappsule.User.SearchForUserIsFriendDTO
 import com.loopj.android.http.AsyncHttpClient
 import com.loopj.android.http.AsyncHttpResponseHandler
-import com.loopj.android.http.RequestParams
 import cz.msebera.android.httpclient.Header
 import cz.msebera.android.httpclient.entity.StringEntity
 import org.json.JSONArray
